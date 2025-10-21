320129f6edc3876924325942c9721656aa9bf57b
nvytska <nvytska@kse.org.ua>
2025-10-21 17:28:14 +0300

    Final fixes

--- proxyServer.sh
+++ proxyServer.sh
@@ -1,9 +1,14 @@
-#!bin/bash
+#!/bin/bash
 set -euo pipefail
 
 A_VAL=$((SOCAT_PID / 3))
 B_VAL=$(cat /proc/stat | head -1 | awk '{print $5}')
 RES=$(( (A_VAL + B_VAL) % 2 ))
+
+echo -e "HTTP/1.1 200 OK\r"
+echo -e "Content-Type: text/html\r"
+echo -e "Connection: close\r"
+echo -e "\r"
 
 if [ $RES -eq 1 ]; then
     cat /var/www/html/index.html
